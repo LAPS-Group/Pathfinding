7d6eb222f95dc9d9c184c747406a7e5a367a925f
Zantox <vetle_er_her@hotmail.com>
2020-04-17 08:37:38 +0000

    Gym env moved to own repo, RL now working decent

--- Ariel/gym-drone/gym_drone/__init__.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id=’DroneCardinal-v0',
-    entry_point=’gym_drone.envs:DroneCardinalDirectionsEnv’
-)
--- Ariel/gym-drone/gym_drone/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from gym_drone.envs.drone_cardinal_directions import DroneCardinalDirectionsEnv
